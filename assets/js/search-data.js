65f51e95f300b27379d3fdee55b2cbdc50b8e82f
Ta1al <talalahmad257@gmail.com>
2025-07-24 15:28:33 +0000

    Deploying to gh-pages from @ Ta1al/ta1al.github.io@dc81aac14eba4aad427b7d6a219d9a60be9cb911 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,12 +52,7 @@ ninja.data = [{
             window.location.href = "/blog/2025/first-post/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
